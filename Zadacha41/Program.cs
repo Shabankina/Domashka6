bbeefc22b365c1cb8c0648dd4c38b80daf462b93
«Marina <marinka377@gmail.com>
2023-07-11 14:44:26 +0300

    Задачи решены

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int[] mas = new int[15];
+
+void FillArray(int[] mas)
+{
+    int lenght = mas.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        mas[index] = new Random (). Next (-100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] mas)
+{
+    int count = mas.Length;
+    int i = 0;
+    while (i < count)
+    {
+        Console.Write (mas[i] + " ");
+        i ++;
+    }
+}
+FillArray (mas);
+PrintArray (mas);
+
+int count = 0;
+int i = 0;
+
+while (i < mas.Length)
+{
+    if (mas[i] > 0)
+    {
+    count = count + 1;
+    }
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine(count);
---
+++ Zadacha43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] a = new double[2] {2, 4};
+double[] b = new double[2] {5, 9};
+
+for (int i = 0; i < a.Length; i++)
+{
+  Console.Write (a[i] + " ");
+}
+Console.WriteLine();
+for (int k = 0; k < b.Length; k++)
+{
+  Console.Write (b[k]+ " ");
+}
+
+double x = -(a[0] - a[1]) / (b[0] - b[1]);
+double y = b[0] * x + a[0];
+
+Console.WriteLine();
+Console.WriteLine($"\n Точка пересечения двух прямых: [{x:f2},{y:f2}]");
